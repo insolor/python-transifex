64f4f1786b9151bec47d16fb70029d31f97ffc15
insolor <insolor@gmail.com>
2017-01-08 21:43:59 +0300

    Make get_translation() return responce.content if path_to_pofile is not supplied (or is None)

--- transifex/api.py
+++ transifex/api.py
@@ -242,7 +242,7 @@ class TransifexAPI(object):
             return json.loads(response.text)
             
     def get_translation(self, project_slug, resource_slug, language_code,
-                        path_to_pofile):
+                        path_to_pofile=None):
         """
         Returns the requested translation, if it exists. The translation is
         returned as a serialized string, unless the GET parameter file is
@@ -274,9 +274,12 @@ class TransifexAPI(object):
         if response.status_code != requests.codes['OK']:
             raise TransifexAPIException(response)
         else:
-            handle = open(output_path, 'wb')
-            handle.write(response.content)
-            handle.close()
+            if path_to_pofile is None:
+                return response.content
+            else:
+                handle = open(output_path, 'wb')
+                handle.write(response.content)
+                handle.close()
             
     def list_languages(self, project_slug, resource_slug):
         """
